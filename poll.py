b0723710fb69e45d91a5e4459bf5175919cb46f4
sarathmlp <sarathmlp@gmail.com>
2017-03-24 08:59:50 +0530

    updated version

--- poll.py
+++ poll.py
@@ -4,9 +4,26 @@ from flask import render_template
 import os
 app = Flask(__name__)
 
+filename = 'data.txt'
+
+poll_data = {
+        'question' : 'Which web framework do you use?',
+        'fields'   : ['Flask', 'Django', 'web2py']
+        }
+
 @app.route('/')
 def root():
-    return render_template('poll.html')
+    return render_template('poll.html', data=poll_data)
+
+@app.route('/poll', methods=['POST'])
+def poll():
+    vote = request.form['field']
+
+    out = open(filename, 'a')
+    out.write(vote + '\n')
+    out.close()
+
+    return render_template('thankyou.html', data=poll_data)
 
 if __name__ == "__main__":
     app.run()
