223917e7f2fe7428cf39e58f9e66573071570365
sarathmlp <sarathmlp@gmail.com>
2017-04-15 12:14:31 +0530

    clean-up

--- unit_test/mongo_test.py
+++ unit_test/mongo_test.py
@@ -1,26 +1,22 @@
+import sys
+sys.path.append( '../')
 from flask import Flask
 import pymongo
 from pymongo import MongoClient
 import pprint
 import ast
 from models import Question
+from bson import ObjectId
 
 client = MongoClient('mongodb://localhost:27017')
-
 db = client.poll
 poll_data = db.poll_data
 
-if poll_data.count() == 0:
-    print ('Creating db')
-    f = open('input.json', 'r')
-    data = f.read()
-    q_list = ast.literal_eval(data)
-    result = poll_data.insert_many(q_list)
-    print (result.inserted_ids)
+cursor = poll_data.find().limit(1)
+for data in cursor:
+    pprint.pprint(data)
+    last_id =  data['_id']
 
-ques = Question()
-for q in poll_data.find():
-    ques.q_list.append(q)
-
-for q in ques.q_list:
-    pprint.pprint(q)
+cursor = poll_data.find({'_id' : {'$gt' : ObjectId(last_id)}}). limit(1)
+for data in cursor:
+    pprint.pprint(data)
